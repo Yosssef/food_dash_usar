be4cb7d4adc5901b9f3c03f24fe304b32b0fa30c
Yosssef <yossseflitef@gmail.com>
2026-09-11 16:24:38 +0300

    Refector ui

--- lib/feature/Restaurant/presentation/widgets/restauant/restaurants_header.dart
+++ lib/feature/Restaurant/presentation/widgets/restauant/restaurants_header.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil_plus/flutter_screenutil_plus.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
